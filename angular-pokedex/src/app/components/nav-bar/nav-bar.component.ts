fc284c8f572c970261579d2b02077576eb78accc
Raphael L <95318121+RaphaelGLv@users.noreply.github.com>
2023-12-22 15:01:57 -0300

    fix: scrollbar + responsive list

--- angular-pokedex/src/app/components/nav-bar/nav-bar.component.ts
+++ angular-pokedex/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ListPokemonComponent } from '../list-pokemon/list-pokemon.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    ListPokemonComponent,
+    FormsModule,
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
